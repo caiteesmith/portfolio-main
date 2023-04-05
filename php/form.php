558c70be726d5ee9e114e7e3630fd22d7bc654a9
Caitee <caiteee@gmail.com>
2023-04-05 15:25:25 -0400

    updated form php

--- php/form.php
+++ php/form.php
@@ -7,23 +7,26 @@ if(
 	//getting customer data
 	$name = $_POST['name']; //getting customer name
 	$email = $_POST['email']; //getting customer email
-	$subject = $_POST['subject']; //getting subject line from client
+	$subject = "Contact Form Details";
  
 	//Email body I will receive
 	$message = "Name: " . $name . "\n"
-	. "Portfolio Message: " . "\n" . $_POST['message'];
+	. "Message: " . "\n" . $_POST['message'];
 
     //Email headers
     $headers = "From: " . $email; // Client email, I will receive
 
     //PHP mailer function
-    $result1 = mail($mailto, $subject, $message, $headers); // This email sent to My address
+    $result = mail($mailto, $subject, $message, $headers); // This email sent to My address
 
     //Checking if mail sent successfully
-    if ($result1 && $result2) {
-      $success = "Your message was sent successfully!";
-    } else {
-      $failed = "Sorry! Your message was not sent. Try again later.";
-    }
+    if ($result) {
+		echo '<script type="text/javascript">alert("Your Message was sent Successfully!");</script>';
+		echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+
+	} else {
+		echo '<script type="text/javascript">alert("Sorry! Message was not sent, Try again Later.");</script>';
+		echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+	}
   }
 ?>
