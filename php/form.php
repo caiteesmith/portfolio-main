19d326b847456270573059262907540f4983ad26
Caitee <caiteee@gmail.com>
2023-04-05 15:36:29 -0400

    updated form php

--- php/form.php
+++ php/form.php
@@ -1,32 +1,57 @@
 <?php  
  
-if(
-	isset($_POST['submit'])) {
-	$mailto = "caiteecodes@gmail.com";
- 
-	//getting customer data
-	$name = $_POST['name']; //getting customer name
-	$email = $_POST['email']; //getting customer email
-	$subject = "Contact Form Details";
- 
-	//Email body I will receive
-	$message = "Name: " . $name . "\n"
-	. "Message: " . "\n" . $_POST['message'];
+$message = ""; 
+if(isset($_POST['submit'])) {
+      $mailto = "caiteecodes@gmail.com";
 
-    //Email headers
-    $headers = "From: " . $email; // Client email, I will receive
+      //getting customer data
+      $name = $_POST['name']; //getting customer name
+      $email = $_POST['email']; //getting customer email
+      $subject = "Contact Form Details";
 
-    //PHP mailer function
-    $result = mail($mailto, $subject, $message, $headers); // This email sent to My address
+      //Email body I will receive
+      $message = "<!DOCTYPE html>
+      <html>
+      <head></head>
+      <body>
+      <table rules='all' border='1' style='border-color: #666;' cellpadding='10'>
+      <tr style='background: #eee;'><td colspan='2'><strong>Contact Form Details</strong> </td></tr>
+      <tr>
+          <td><strong>Name:</strong></td>
+          <td>".$_POST['name']."</td>
+      </tr>
+      <tr>
+          <td><strong>Email:</strong></td>
+          <td>".$_POST['email']."</td>
+      </tr>
+      <tr>
+          <td><strong>Message:</strong></td>
+          <td>".$_POST['message']."</td>
+      </tr>
+      </table>
+      </body>
+      </html>";
 
-    //Checking if mail sent successfully
-    if ($result) {
-		echo '<script type="text/javascript">alert("Your message was sent successfully!");</script>';
-		echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+      $subject = "Contact Form Details";
 
-	} else {
-		echo '<script type="text/javascript">alert("Your message was not sent. Please try again Later.");</script>';
-		echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+      // Set content-type header for sending HTML email 
+      $headers = "MIME-Version: 1.0" . "\r\n"; 
+      $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+      $headers .= "From:" . $email . "\r\n";
+
+      $result = mail($to,$subject,$message,$headers);
+
+      if ($result) {
+          // $message = "Your Message was sent Successfully!";
+          echo '<script type="text/javascript">alert("Your message was sent successfully!");</script>';
+          echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
+
+      } else{
+          // $message = "Sorry! Message was not sent, Try again Later.";
+          echo '<script type="text/javascript">alert("Your message was not sent. Try again Later.");</script>';
+          echo '<script type="text/javascript">window.location.href = window.location.href;</script>';
 	}
-  }
+}
+
 ?>
