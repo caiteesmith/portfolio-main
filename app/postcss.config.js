cfae6561b3c2491e7629b55af44560c3e1089f3e
csm3254 <csm3254@wgu.edu>
2025-09-30 16:55:30 -0400

    Update tailwind, remove webmanifest

--- app/postcss.config.js
+++ app/postcss.config.js
@@ -1,7 +1,6 @@
-// app/postcss.config.js
-import tailwind from '@tailwindcss/postcss';
-import autoprefixer from 'autoprefixer';
-
 export default {
-  plugins: [tailwind(), autoprefixer()],
-};
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {}
+  }
+}
